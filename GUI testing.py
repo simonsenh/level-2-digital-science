2d897faf43c5ffd685e5e369e3272affd2012e8b
simonsenh <simonsenh@stu.otc.school.nz>
2022-09-20 10:50:51 +1200

    Display functionallity for budgeting

--- GUI testing.py
+++ GUI testing.py
@@ -391,6 +391,7 @@ def calculate_budget():
     counter_3 = 1
     expenses_list = []
     spending_ratios = [['needs', 0.5], ['wants', 0.3], ['savings', 0.2]]
+    needs_spending_ratios = [["Housing", 0.2], ["Transportation", 0.1], ["Food", 0.2], ["Utilities", 0.2], ["Insurance", 0.1], ["Medical & Healthcare", 0.2]]
     needs_amount = 0
     wants_amount = 0
     savings_amount = 0
@@ -411,6 +412,33 @@ def calculate_budget():
             needs_amount += expenses_list[counter_3][1]
         counter_3 += 1
     print(needs_amount, wants_amount, savings_amount)
+    y1 = round(needs_amount - spending_ratios[0][1] * total_amount, ndigits=2)
+    y2 = round(spending_ratios[0][1] * total_amount - needs_amount, ndigits=2)
+    y3 = round(wants_amount - spending_ratios[1][1] * total_amount, ndigits=2)
+    y4 = round(spending_ratios[1][1] * total_amount - wants_amount, ndigits=2)
+    y5 = round(savings_amount - spending_ratios[2][1] * total_amount, ndigits=2)
+    y6 = round(spending_ratios[2][1] * total_amount - savings_amount, ndigits=2)
+    if spending_ratios[0][1] * total_amount < needs_amount:
+        needs_ratio_label.config(text="you can reduce spending on needs by {}$".format(y1))
+    else:
+        needs_ratio_label.config(text="you can increase spending on needs by {}$".format(y2))
+    if spending_ratios[1][1] * total_amount < wants_amount:
+        wants_ratio_label.config(text="you can reduce spending on wants by {}$".format(y3))
+    else:
+        wants_ratio_label.config(text="you can increase spending on wants by {}$".format(y4))
+    if spending_ratios[2][1] * total_amount < savings_amount:
+        savings_ratio_label.config(text="you can reduce spending on savings by {}$".format(y5))
+    else:
+        savings_ratio_label.config(text="you can increase spending on savings by {}$".format(y6))
+    counter_3 = 0
+    while counter_3 < 6:
+        y = needs_spending_ratios[counter_3][0]
+        if needs_spending_ratios[counter_3][1] * needs_amount > calculate[which_user][4][counter_3 + 1]/calculate[which_user][5][counter_3 + 1]:
+            housing_label.config(text="you can increase spending on {} by {}$".format(needs_spending_ratios[counter_3][0], needs_spending_ratios[counter_3][1] * needs_amount - calculate[which_user][4][counter_3 + 1]/calculate[which_user][5][counter_3 + 1]))
+        else:
+            housing_label.config(text="you can decrease spending on {} by {}$".format(needs_spending_ratios[counter_3][0], calculate[which_user][4][counter_3 + 1]/calculate[which_user][5][counter_3 + 1] - needs_spending_ratios[counter_3][1] * needs_amount))
+        counter_3 += 1
+
 
 
 def which_user_func():
@@ -589,6 +617,33 @@ cancel_button_budget.grid(row=0, column=0)
 
 calculate_budget_button = ttk.Button(budget, text="calculate budget", width=20, command=lambda: calculate_budget())
 calculate_budget_button.grid(row=0, column=1)
+
+needs_ratio_label = ttk.Label(budget)
+needs_ratio_label.grid(row=1, column=0)
+
+wants_ratio_label = ttk.Label(budget)
+wants_ratio_label.grid(row=2, column=0)
+
+savings_ratio_label = ttk.Label(budget)
+savings_ratio_label.grid(row=3, column=0)
+
+housing_label = ttk.Label(budget)
+housing_label.grid(row=4, column=0)
+
+transport_label = ttk.Label(budget)
+transport_label.grid(row=5, column=0)
+
+food_label = ttk.Label(budget)
+food_label.grid(row=6, column=0)
+
+utilitys_label = ttk.Label(budget)
+utilitys_label.grid(row=7, column=0)
+
+insurance_label = ttk.Label(budget)
+insurance_label.grid(row=8, column=0)
+
+medical_label = ttk.Label(budget)
+medical_label.grid(row=9, column=0)
 
 # tips
 cancel_button_tips = ttk.Button(tips, text="cancel", width=10, command=lambda: go_main_menu(tips))
